57ee1e084a04d20e70a51172a63d00805424e969
jackalcooper <jackalcooper@gmail.com>
2016-12-17 18:03:00 +0800

    finishing

--- lib/thunder_belt.ex
+++ lib/thunder_belt.ex
@@ -23,8 +23,33 @@ defmodule ThunderBelt do
     end
 
     def update_record(accelerate_task) do
-        user_data_new = accelerate_task."UserData"
-        changeset = AccelerateTask.changeset(accelerate_task, %{"UserData": user_data_new})
-        |> IO.inspect
+        user_data = accelerate_task."UserData" |> Poison.decode! 
+        # |> IO.inspect
+        
+        case user_data["Result"] do
+           0  ->
+               IO.puts "No need for updating"
+               false
+           _ ->
+               IO.puts "Updating"
+               user_data_new = Map.put(user_data, "Result", 0)
+            #    |> IO.inspect
+               user_data_str_new = Poison.encode!(user_data_new)
+               changeset = AccelerateTask.changeset(accelerate_task, %{"UserData": user_data_str_new})
+            #    IO.puts "changes:"
+            #    changeset.changes |> IO.inspect
+               Repo.update(changeset)
+               |> IO.inspect
+        end
+    end
+
+    def need_rectification(user_data) do
+        user_data = user_data |> Poison.decode!
+        case user_data["Result"] do
+           0  ->
+               false
+           _ ->
+               true
+        end
     end
 end
--- test/thunder_belt_test.exs
+++ test/thunder_belt_test.exs
@@ -6,4 +6,9 @@ defmodule ThunderBeltTest do
     ThunderBelt.q |> List.last |> ThunderBelt.update_record
     assert 1 + 1 == 2
   end
+
+  # test "need_rectification" do
+  #   ThunderBelt.q |> Enum.map(&ThunderBelt.need_rectification/1)
+  #   |> IO.inspect
+  # end
 end
