41cfe68f0c447e49d5a803aefd4878367806932b
jackalcooper <jackalcooper@gmail.com>
2016-12-17 20:09:12 +0800

    save

--- lib/thunder_belt.ex
+++ lib/thunder_belt.ex
@@ -26,14 +26,21 @@ defmodule ThunderBelt do
         user_data = accelerate_task."UserData" |> Poison.decode! 
         # |> IO.inspect
         
-        user_data_new = Map.put(user_data, "Result", 0)
-    #    |> IO.inspect
-        user_data_str_new = Poison.encode!(user_data_new)
-        changeset = AccelerateTask.changeset(accelerate_task, %{"UserData": user_data_str_new})
-    #    IO.puts "changes:"
-    #    changeset.changes |> IO.inspect
-        Repo.update(changeset)
-        |> IO.inspect
+        case user_data["Result"] do
+           0  ->
+               IO.puts "No need for updating"
+               false
+           _ ->
+               IO.puts "Updating"
+               user_data_new = Map.put(user_data, "Result", 0)
+            #    |> IO.inspect
+               user_data_str_new = Poison.encode!(user_data_new)
+               changeset = AccelerateTask.changeset(accelerate_task, %{"UserData": user_data_str_new})
+            #    IO.puts "changes:"
+            #    changeset.changes |> IO.inspect
+               Repo.update(changeset)
+               |> IO.inspect
+        end
     end
 
     def need_rectification(user_data) do
